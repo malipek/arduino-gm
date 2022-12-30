906df7d2c5d86a32204219df23ff5f49d4aa7303
malipek <malipke00@gmail.com>
2022-12-30 21:00:08 +0100

    Python serial listener on Arduino port

---
+++ SERIAL-LISTENER/listener.py
@@ -0,0 +1,11 @@
+import boto3
+import json
+import serial
+ser = serial.Serial('/dev/ttyACM0')
+ser.flushInput()
+while True:
+    ser_bytes = ser.readline()
+    decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
+    client = boto3.client('lambda')
+    response = client.invoke( FunctionName = "put-records", InvocationType = "RequestResponse", Payload = decoded_bytes)
+    print(response['Payload'].read())
