014b2390f6facdd5452d31de2665396afb0f2f76
malipek <malipke00@gmail.com>
2023-08-26 22:31:58 +0200

    Add second datasource for precipitation (main one always returns zero)

--- SERIAL-LISTENER/listener.py
+++ SERIAL-LISTENER/listener.py
@@ -8,16 +8,26 @@ ser.flushInput()
 while True:
     ser_bytes = ser.readline()
     decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
-    url = os.environ.get('URL')
-    if url:
-        response = requests.get(url)
+    url1 = os.environ.get('URL1')
+    url2 = os.environ.get('URL2')
+    if url1:
+        response = requests.get(url1)
         if response.status_code == 200:
             data = response.text.strip()
             data = data.strip('ï»¿')
             data_json = json.loads(data)
             decoded_bytes = decoded_bytes.replace('}', '')
             decoded_bytes = decoded_bytes + ',"temp2":'+data_json[0]['temp2m']
-            decoded_bytes = decoded_bytes + ',"precipitation":'+data_json[0]['precipitation']
+            decoded_bytes = decoded_bytes + '}'
+    if url2:
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(url2, headers=headers)
+        if response.status_code == 200:
+            data = response.text.strip()
+            data = data.strip('ï»¿')
+            data_json = json.loads(data)
+            decoded_bytes = decoded_bytes.replace('}', '')
+            decoded_bytes = decoded_bytes + ',"precipitation":'+str(data_json["status"]["precip"]['value'])
             decoded_bytes = decoded_bytes + '}'
     client = boto3.client('lambda')
     response = client.invoke( FunctionName = "put-records", InvocationType = "RequestResponse", Payload = decoded_bytes)
