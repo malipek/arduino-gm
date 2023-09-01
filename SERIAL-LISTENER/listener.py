971534983e34699c1c6f89302cffb7fc7c0c4cb1
Pawel Malita <pawel.malita@netguru.com>
2023-09-01 07:35:06 +0200

    Added exception handling for HTTPS requests

--- SERIAL-LISTENER/listener.py
+++ SERIAL-LISTENER/listener.py
@@ -11,24 +11,30 @@ while True:
     url1 = os.environ.get('URL1')
     url2 = os.environ.get('URL2')
     if url1:
-        response = requests.get(url1)
-        if response.status_code == 200:
-            data = response.text.strip()
-            data = data.strip('ï»¿')
-            data_json = json.loads(data)
-            decoded_bytes = decoded_bytes.replace('}', '')
-            decoded_bytes = decoded_bytes + ',"temp2":'+data_json[0]['temp2m']
-            decoded_bytes = decoded_bytes + '}'
+        try:
+            response = requests.get(url1)
+            if response.status_code == 200:
+                data = response.text.strip()
+                data = data.strip('ï»¿')
+                data_json = json.loads(data)
+                decoded_bytes = decoded_bytes.replace('}', '')
+                decoded_bytes = decoded_bytes + ',"temp2":'+data_json[0]['temp2m']
+                decoded_bytes = decoded_bytes + '}'
+        except requests.exceptions.RequestException as e:
+            print(e)
     if url2:
-        headers = {'Content-Type': 'application/json'}
-        response = requests.get(url2, headers=headers)
-        if response.status_code == 200:
-            data = response.text.strip()
-            data = data.strip('ï»¿')
-            data_json = json.loads(data)
-            decoded_bytes = decoded_bytes.replace('}', '')
-            decoded_bytes = decoded_bytes + ',"precipitation":'+str(data_json["status"]["precip"]['value'])
-            decoded_bytes = decoded_bytes + '}'
+        try:
+            headers = {'Content-Type': 'application/json'}
+            response = requests.get(url2, headers=headers)
+            if response.status_code == 200:
+                data = response.text.strip()
+                data = data.strip('ï»¿')
+                data_json = json.loads(data)
+                decoded_bytes = decoded_bytes.replace('}', '')
+                decoded_bytes = decoded_bytes + ',"precipitation":'+str(data_json["status"]["precip"]['value'])
+                decoded_bytes = decoded_bytes + '}'
+        except requests.exceptions.RequestException as e:
+            print(e)
     client = boto3.client('lambda')
     response = client.invoke( FunctionName = "put-records", InvocationType = "RequestResponse", Payload = decoded_bytes)
     print(decoded_bytes+'\n')
